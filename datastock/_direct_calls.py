4839e969344bb192623177e20d58b786f21f536f
Didou09 <didier.vezineT@gmail.com>
2022-03-01 19:23:33 +0100

    [#003] Implemented direct call too

--- datastock/_direct_calls.py
+++ datastock/_direct_calls.py
@@ -7,7 +7,10 @@ import numpy as np
 from ._class import DataStock
 
 
-__all__ = ['plot_as_array']
+__all__ = [
+    'plot_as_array',
+    'plot_BvsA_as_distribution',
+]
 
 
 # #############################################################################
@@ -40,3 +43,34 @@ def plot_as_array(data=None):
     st.add_data(data)
 
     return st.plot_as_array(inplace=True)
+
+
+def plot_BvsA_as_distribution(dataA=None, dataB=None):
+    """ Interactive plotting of any 2d np.ndarray """
+
+    # ------------
+    # check inputs
+
+    try:
+        dataA = np.asarray(dataA)
+        dataB = np.asarray(dataB)
+        assert dataA.ndim == 1
+        assert dataA.shape == dataB.shape
+    except Exception as err:
+        msg = (
+            str(err)
+            + "\nArg dataA and dataB must be 1d np.ndarray of same shape!\n"
+            f"Provided:\n{dataA}\n{dataB}"
+        )
+        raise Exception(msg)
+
+    # ---------------------
+    # Instanciate datastock
+
+    st = DataStock()
+    st.add_data(key='dataA', data=dataA)
+    st.add_data(key='dataB', data=dataB)
+
+    return st.plot_BvsA_as_distribution(
+        keyA='dataA', keyB='dataB', inplace=True,
+    )
