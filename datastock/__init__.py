10d624c5efe71d960adb54acdc9d0a5308a219a8
dvezinet <didier.vezinet@gmail.com>
2025-03-30 20:01:48 +0200

    [#187] Progressing, not there yet

--- datastock/__init__.py
+++ datastock/__init__.py
@@ -1,6 +1,9 @@
 
 
-from .version import __version__
+# ###############
+# sub-packages
+# ###############
+
 
 from . import _generic_check
 from ._generic_utils_plot import *
@@ -8,3 +11,19 @@ from ._class import DataStock
 from ._saveload import load, get_files
 from ._direct_calls import *
 from . import tests
+
+
+# ###############
+# __version__
+# ###############
+
+
+from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version("package-name")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+#
+# cleanup
+del version, PackageNotFoundError
