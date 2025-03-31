152c164feca978e6ece31c6ae5d9b73fed6e6a1b
dvezinet <didier.vezinet@gmail.com>
2025-03-31 04:03:47 +0200

    [#187] __version__ hard-coded in __init__.py

--- datastock/__init__.py
+++ datastock/__init__.py
@@ -1,3 +1,16 @@
+# ###############
+# __version__
+# ###############
+
+
+__version__ = "0.0.50"
+# from setuptools_scm import get_version
+# __version__ = get_version(root='..', relative_to=__file__)
+
+# from importlib.metadata import version
+# __version__ = version(__package__)
+# cleanup
+# del get_version
 
 
 # ###############
@@ -11,19 +24,3 @@ from ._class import DataStock
 from ._saveload import load, get_files
 from ._direct_calls import *
 from . import tests
-
-
-# ###############
-# __version__
-# ###############
-
-
-from importlib.metadata import version, PackageNotFoundError
-try:
-    __version__ = version("package-name")
-except PackageNotFoundError:
-    # package is not installed
-    pass
-#
-# cleanup
-del version, PackageNotFoundError
