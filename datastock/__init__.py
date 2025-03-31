33512cdd0c4435ce30c88bb544bc5da84eacaa69
dvezinet <didier.vezinet@gmail.com>
2025-03-31 05:43:46 +0200

    [#187] uv build working locally

--- datastock/__init__.py
+++ datastock/__init__.py
@@ -3,9 +3,14 @@
 # ###############
 
 
-__version__ = "0.0.50"
+from . import _version
+__version__ = _version.version
+__version_tuple__ = _version.version_tuple
+
+
 # from setuptools_scm import get_version
 # __version__ = get_version(root='..', relative_to=__file__)
+
 
 # from importlib.metadata import version
 # __version__ = version(__package__)
